558dd95e8130717970925c224d061b77a9ed5f9b
Nandish Jayaram <njayaram@apache.org>
2017-02-24 12:19:27 -0800

    Use a jenkins specific docker image in jenkins_build.sh
    Created a new docker image which does not have make doc specific
    depedencies. Use that image in jenkins build.

--- tool/jenkins/jenkins_build.sh
+++ tool/jenkins/jenkins_build.sh
@@ -9,14 +9,14 @@
 #####################################################################################
 
 # Pull down the base docker images
-docker pull madlib/postgres_9_6:latest
+docker pull madlib/postgres_9_6:jenkins
 # Assuming git clone of incubator-madlib has been done, launch a container with the volume mounted
-docker run -d --name madlib -v incubator-madlib:/incubator-madlib madlib/postgres_9.6:latest
+docker run -d --name madlib -v incubator-madlib:/incubator-madlib madlib/postgres_9.6:jenkins
 ## This sleep is required since it takes a couple of seconds for the docker
 ## container to come up, which is required by the docker exec command that follows.
 sleep 5
 # cmake, make and make install MADlib
-docker exec madlib -c 'mkdir /incubator-madlib/build ; cd /incubator-madlib/build ; cmake .. ; make ; make install'
+docker exec madlib bash -c 'mkdir /incubator-madlib/build ; cd /incubator-madlib/build ; cmake .. ; make ; make install'
 # Install MADlib and run install check
 docker exec -it madlib /incubator-madlib/build/src/bin/madpack -p postgres -c postgres/postgres@localhost:5432/postgres install
 docker exec -it madlib /incubator-madlib/build/src/bin/madpack -p postgres  -c postgres/postgres@localhost:5432/postgres install-check
