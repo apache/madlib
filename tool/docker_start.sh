ae09ff3bf635b1525095fec65530259a76285c21
Jingyi Mei <jmei@pivotal.io>
2017-09-12 16:40:47 -0700

    Modify scripts based on code review comment

--- tool/docker_start.sh
+++ tool/docker_start.sh
@@ -30,8 +30,8 @@
 #####################################################################################
 set -o pipefail
 
-workdir=`pwd`
-user_name=`whoami`
+workdir=$(pwd)
+user_name=$(whoami)
 reponame=madlib
 
 echo "======================================================================"
@@ -39,31 +39,19 @@ echo "Build user: $user_name"
 echo "Work directory: $workdir"
 echo "Git reponame: $reponame"
 echo "======================================================================"
-echo "ls -la"
-ls -la
-echo "rm -rf logs"
 rm -rf logs
-echo "mkdir logs"
 mkdir logs
 
 echo "-----------creating docker container madlib--------------------"
-echo "docker kill madlib"
-docker kill madlib-test
-echo "docker rm madlib"
-docker rm madlib-test
+docker kill madlib
+docker rm madlib
 
 # Pull down the base docker images
 echo "Creating docker container"
 docker pull madlib/postgres_9.6:latest
 
 # Launch docker container with volume mounted from workdir
-echo "-------------------------------"
-cat <<EOF
-docker run -d --name madlib-test -v "${workdir}/${reponame}":/madlib madlib/postgres_9.6 | tee logs/docker_setup.log
-EOF
-docker run -d --name madlib-test -v "${workdir}/${reponame}":/madlib madlib/postgres_9.6 | tee logs/docker_setup.log
-echo "-------------------------------"
-
+docker run -d --name madlib -v "${workdir}/${reponame}":/madlib madlib/postgres_9.6 | tee logs/docker_setup.log
 
 ## This sleep is required since it takes a couple of seconds for the docker
 ## container to come up, which is required by the docker exec command that follows.
@@ -71,29 +59,17 @@ sleep 5
 
 echo "---------- Building MADlib -----------"
 # cmake, make, make install
-cat <<EOF
-docker exec madlib bash -c 'rm -rf /build; mkdir /build; cd /build; cmake ../madlib; make clean; make; make install' | tee $workdir/logs/madlib_compile.log
-EOF
-docker exec madlib-test bash -c 'rm -rf /build; mkdir /build; cd /build; cmake ../madlib; make clean; make; make install' | tee $workdir/logs/madlib_compile.log
+docker exec madlib bash -c 'rm -rf /build; mkdir /build; cd /build; cmake ../madlib; make; make install' | tee $workdir/logs/madlib_compile.log
 
 echo "---------- Installing and running install-check --------------------"
 # Install MADlib and run install check
-cat <<EOF
 docker exec madlib bash -c '/build/src/bin/madpack -p postgres -c postgres/postgres@localhost:5432/postgres install' | tee $workdir/logs/madlib_install.log
-EOF
-docker exec madlib-test bash -c '/build/src/bin/madpack -p postgres -c postgres/postgres@localhost:5432/postgres install' | tee $workdir/logs/madlib_install.log
 
-cat <<EOF
 docker exec madlib bash -c 'mkdir -p /tmp'
 docker exec madlib bash -c '/build/src/bin/madpack -p postgres  -c postgres/postgres@localhost:5432/postgres -d /tmp install-check' | tee $workdir/logs/madlib_install_check.log
-EOF
-
-docker exec madlib-test bash -c 'mkdir -p /tmp'
-docker exec madlib-test bash -c '/build/src/bin/madpack -p postgres  -c postgres/postgres@localhost:5432/postgres -d /tmp install-check' | tee $workdir/logs/madlib_install_check.log
 
 # To run psql, you have to login as postgres, not root
 echo "---------- docker exec image as user postgres-----------"
-cat <<EOF
-docker exec -it --user=postgres madlib bash | tee $workdir/logs/docker_exec.log
-EOF
-docker exec --user=postgres -it madlib-test bash | tee $workdir/logs/docker_exec.log
+docker exec madlib bash -c 'chown -R postgres:postgres /build'
+docker exec madlib bash -c 'chown -R postgres:postgres /madlib'
+docker exec --user=postgres -it madlib bash | tee $workdir/logs/docker_exec.log
