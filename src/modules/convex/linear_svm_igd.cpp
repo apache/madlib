ceab57f31ddf15a1de8621a22633e052ba0028ff
Rahul Iyer <riyer@apache.org>
2018-07-10 13:47:39 -0700

    SVM: Compute average loss per row instead of total loss

--- src/modules/convex/linear_svm_igd.cpp
+++ src/modules/convex/linear_svm_igd.cpp
@@ -192,7 +192,7 @@ internal_linear_svm_igd_result::run(AnyType &args) {
 
     AnyType tuple;
     tuple << state.task.model
-        << static_cast<double>(state.algo.loss)
+        << static_cast<double>(state.algo.loss / state.algo.numRows)
         << state.algo.gradient.norm()
         << static_cast<int64_t>(state.algo.numRows);
 
